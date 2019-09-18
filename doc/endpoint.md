d70b19f358e29ae50a2ce2872df856fd38ec54e3
jianhua.xiang <9547457@qq.com>
2019-09-18 12:50:17 +0800

    first commit

---
+++ doc/endpoint.md
@@ -0,0 +1,13 @@
+# Api Endpoints for Broker
+
+## Base Domain
+
+cococoin.com
+
+## API Endpoint ($HOST)
+
+| Name               | base endpoint                                    |
+| ------------------ | ------------------------------------------------ |
+| rest-api           | **[https://api.cococoin.com](https://api.cococoin.com)** |
+| web-socket-streams | **[wss://wsapi.cococoin.com](wss://wsapi.cococoin.com)** |
+| user-data-stream   | **[wss://wsapi.cococoin.com](wss://wsapi.cococoin.com)** |
---
+++ sdk/java/src/test/java/io/bhex/api/test/constant/Constants.java
@@ -0,0 +1,14 @@
+package io.bhex.api.test.constant;
+
+public class Constants {
+
+    // FIXME: change to a valid key and secret
+    //for cococoin.com
+    public final static String ACCESS_KEY = "rKj8JXuFAoCQX4IByQoQHHxJNcJjysXCbAokT3B3C5mvzNmTdcb2B8YglHn7iKGG";
+    public final static String SECRET_KEY = "yfjPJxiM2kdhNMHUF8FO8UHHxfm0Jtro3ex8NTwe7jkPfS30pdOyqCHpe1JG1PQT";
+
+    //for bhex
+//    public final static String ACCESS_KEY = "7cKTzyrVpx1mrp9ECGqjaVcqcI96WOrWym3Ar3GYfTNMfdGL6bo8f9ZGE2MQEEB4";
+//    public final static String SECRET_KEY = "WFFy4Ic3nuM6U8QA9aCIWwgUoZE62HdZSAcv7NAnqUclsGRaeQrQoavv0rONNtJX";
+
+}
